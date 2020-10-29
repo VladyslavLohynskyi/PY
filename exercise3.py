36c11207900e8616ffebc21e0e6b6432b3a4bc86
watcher2003 <watcher.good2003@gmail.com>
2020-10-29 12:23:28 +0200

    Add files via upload

---
+++ exercise3.py
@@ -0,0 +1,5 @@
+y=input("Enter your word:")
+for i in y:
+    if i=="":
+        continue
+    print(2*i, end='')
---
+++ exercise4.py
@@ -0,0 +1,71 @@
+print('HALLO\nCongatulations, in our calculator')
+
+print("If you want finish write 'STOP'")
+Dia = input("Enter operation: ")
+
+
+
+
+import sys
+
+def sum(a,b):
+
+    print("{}+{}={:.1f}".format(a,b,float(a)+float(b)))
+
+def riz(a,b):
+    print("{}-{}={:.1f}".format(a,b,float(a)-float(b)))
+
+def dob(a,b):
+    print("{}*{}={:.1f}".format(a,b,float(a)*float(b)))
+
+def chas(a,b):
+    print("{}/{}={:.1f}".format(a,b,float(a)/float(b)))
+
+while True:
+
+    if Dia == '+':
+        a = input("Enter First Number: ")
+        b = input('Enter Second Number: ')
+        if a== "STOP" or b== "STOP":
+            print("Program finished!")
+            sys.exit()
+        else:
+            sum(a, b)
+    elif Dia == "STOP":
+        sys.exit()
+    elif Dia == '-':
+        a = input("Enter First Number: ")
+        b = input('Enter Second Number: ')
+        if a== "STOP" or b== "STOP":
+            print("Program finished!")
+            sys.exit()
+        else:
+            riz(a,b)
+    elif Dia == '*':
+        a = input("Enter First Number: ")
+        b = input('Enter Second Number: ')
+        if a== "STOP" or b== "STOP":
+            print("Program finished!")
+            sys.exit()
+        if a== "STOP" or b== "STOP":
+            print("Program finished!")
+            sys.exit()
+        else:
+            dob(a,b)
+    elif Dia == '/':
+        a = input("Enter First Number: ")
+        b = input('Enter Second Number: ')
+        if a== "STOP" or b== "STOP":
+            print("Program finished!")
+            sys.exit()
+        else:
+            try:
+                chas(a,b)
+            except:
+                print("Даун! на "+ str(b) + " ділити не можна!")
+
+    else:
+        print('Operation did not found!')
+        sys.exit()
+
+
